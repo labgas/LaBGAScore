980544c80ab95f22c7d8b7f99948bb14017f35d3
lukasvo76 <lukas.vanoudenhove@kuleuven.be>
2023-09-18 16:49:43 +0200

    improvements in layout for publishing and masking code

--- firstlevel/LaBGAScore_firstlevel_s3_diagnose_model.m
+++ firstlevel/LaBGAScore_firstlevel_s3_diagnose_model.m
@@ -1,27 +1,35 @@
-%%% LaBGAScore_firstlevel_s3_diagnose_model.m
+%% LaBGAScore_firstlevel_s3_diagnose_model.m
 %
 % This script will run diagnostics on first level models and publish them
 % as an html report using the CANlab function scn_spm_design_check, and
 % save variance inflation factors as a .mat file
 %
-% DOCUMENTATION
-% help scn_spm_design_check
+%
+% *DOCUMENTATION*
 % 
-% USAGE
+% type "help scn_spm_design_check" in Matlab command window
+%
+%
+% *USAGE*
+%
 % Script should be called from LaBGAScore_firstlevel_s2_fit_model.m, it
 % is not for standalone use
 %
-%__________________________________________________________________________
+% -------------------------------------------------------------------------
 %
 % authors: Lukas Van Oudenhove
+%
 % date:   March, 2022
 %
-%__________________________________________________________________________
-% @(#)% LaBGAScore_firstlevel_s3_diagnose_model.m         v1.2        
-% last modified: 2022/05/29
-
-
+% -------------------------------------------------------------------------
+%
+% LaBGAScore_firstlevel_s3_diagnose_model.m         v1.3   
+%
+% last modified: 2023/03/24
+%
+%
 %% VIFS AND DESIGN
+% -------------------------------------------------------------------------
 
     vifs = scn_spm_design_check(subjfirstdir,'events_only','vif_thresh',LaBGAS_options.mandatory.vif_thresh);
     drawnow, snapnow
@@ -30,6 +38,7 @@
     
 
 %% FIRST LEVEL MAPS
+% -------------------------------------------------------------------------
 
 if LaBGAS_options.display.plotmontages
 
@@ -42,13 +51,17 @@ if LaBGAS_options.display.plotmontages
 
     [~,maskname,maskext] = fileparts(LaBGAS_options.display.mask);
     mask = [maskname,maskext];
+    mask_img = fmri_mask_image(LaBGAS_options.display.mask,'noverbose');
+    target = fullfile(tmapnames(1).folder,tmapnames(1).name);
+    mask_img = resample_space(mask_img,target); % resample mask space to T-map space
+    mask_img.dat(mask_img.dat > 0) = 1; % binarize resampled mask
 
     fprintf('\nShowing results at p < %s %s, k = %s, mask = %s\n',num2str(LaBGAS_options.display.input_threshold),LaBGAS_options.display.thresh_type,num2str(LaBGAS_options.display.k),mask); 
 
         for tmap = 1:size(tmapnames,1)
             tmapspaths{tmap} = fullfile(tmapnames(tmap).folder,tmapnames(tmap).name);
             tmapsobj{tmap} = statistic_image('image_names',tmapspaths{tmap},'type','T','dfe',SPM.xX.erdf);
-            tmapsobj{tmap} = threshold(tmapsobj{tmap},LaBGAS_options.display.input_threshold,LaBGAS_options.display.thresh_type,'k',LaBGAS_options.display.k,'mask',LaBGAS_options.display.mask);
+            tmapsobj{tmap} = threshold(tmapsobj{tmap},LaBGAS_options.display.input_threshold,LaBGAS_options.display.thresh_type,'k',LaBGAS_options.display.k,'mask',mask_img);
             create_figure('fmridisplay');
             wh_montage = 5;
             axis off
